c1b58ddeccff9e52ab48936f114f44e288569a6f
Korch <stephaniekorch@quickenloans.com>
2021-06-09 23:06:51 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let name1 = "Paul";
+let name2 = "Pole";
+let name3 = "PIkachu";
+
+if (name1.length === name2.length === name3.length) {
+    console.log(`${name1}, ${name2}, and ${name3} are the same length.`)
+
+} else if (name1.length === name2.length + !name3.length) {
+    console.log(`${name1} and ${name2} tie for the longest name.`)
+
+} else if (name1.length === name3.length + !name2.length) {
+    console.log(`${name1} and ${name3} tie for the longest name.`)
+
+} else if (name1.length > name2.length && name3.length) {
+    console.log(name1 + " has the longest name.")
+
+} else if (name2.length > name1.length && name3.length) {
+    console.log(name2 + " has the longest name.")
+
+} else if (name3.length > name1.length && name2.length) {
+    console.log(name3 + " has the longest name.")
+ 
+}
